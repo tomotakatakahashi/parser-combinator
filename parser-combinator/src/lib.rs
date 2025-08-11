fbbff0d987af105b8c8a2002779139492e032df6
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2025-08-11 15:42:22 +0900

    Remove add and it_works

--- parser-combinator/src/lib.rs
+++ parser-combinator/src/lib.rs
@@ -131,19 +131,9 @@ impl Parser<u32> for Nat {
     }
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn item() {
