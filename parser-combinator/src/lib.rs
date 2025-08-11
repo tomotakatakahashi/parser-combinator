40eac80de7ac6f997746b5f88befb57f526f2b25
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2025-08-11 15:07:41 +0900

    WIP

--- parser-combinator/src/lib.rs
+++ parser-combinator/src/lib.rs
@@ -1,16 +1,55 @@
 trait Parser<T> {
-    fn parse<'a>(&self, input: &'a str) -> Vec<(T, &'a str)>;
+    fn parse<'a>(&self, input: &'a str) -> Option<(T, &'a str)>;
 }
 
 struct Item;
 
 impl Parser<char> for Item {
-    fn parse<'a>(&self, input: &'a str) -> Vec<(char, &'a str)> {
+    fn parse<'a>(&self, input: &'a str) -> Option<(char, &'a str)> {
         let mut chars = input.chars();
         return match chars.next() {
-            Some(c) => return vec![(c, chars.as_str())],
-            None => vec![],
+            Some(c) => return Some((c, chars.as_str())),
+            None => None,
         };
+    }
+}
+
+struct Sat<F>
+where
+    F: Fn(char) -> bool,
+{
+    predicate: F,
+}
+
+impl<F> Parser<char> for Sat<F>
+where
+    F: Fn(char) -> bool,
+{
+    fn parse<'a>(&self, input: &'a str) -> Option<(char, &'a str)> {
+        let item = Item {};
+        let item_result = item.parse(input);
+        return match item_result {
+            None => None,
+            Some(out) => {
+                if (self.predicate)(out.0) {
+                    item_result
+                } else {
+                    None
+                }
+            }
+        };
+    }
+}
+
+fn digit() -> Sat<impl Fn(char) -> bool> {
+    Sat {
+        predicate: |c| c.is_ascii_digit(),
+    }
+}
+
+fn char_parser(target: char) -> Sat<impl Fn(char) -> bool> {
+    Sat {
+        predicate: move |c: char| c == target,
     }
 }
 
@@ -28,10 +67,26 @@ mod tests {
         assert_eq!(result, 4);
     }
 
+    #[test]
     fn item() {
         let input = "abc";
         let item = Item {};
         let result = item.parse(input);
-        assert_eq!(result, vec![('a', "bc")]);
+        assert_eq!(result, Some(('a', "bc")));
+    }
+
+    #[test]
+    fn digit_works() {
+        let digit = digit();
+
+        assert_eq!(digit.parse("123"), Some(('1', "23")));
+        assert_eq!(digit.parse("abc"), None);
+    }
+
+    #[test]
+    fn char_parser_works() {
+        let parser = char_parser('a');
+        assert_eq!(parser.parse("abc"), Some(('a', "bc")));
+        assert_eq!(parser.parse("123"), None);
     }
 }
