c98325c6e16630c4282966a05fe15206d72f24bb
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2025-08-11 11:40:19 +0900

    Add Item

--- parser-combinator/src/lib.rs
+++ parser-combinator/src/lib.rs
@@ -1,3 +1,19 @@
+trait Parser<T> {
+    fn parse<'a>(&self, input: &'a str) -> Vec<(T, &'a str)>;
+}
+
+struct Item;
+
+impl Parser<char> for Item {
+    fn parse<'a>(&self, input: &'a str) -> Vec<(char, &'a str)> {
+        let mut chars = input.chars();
+        return match chars.next() {
+            Some(c) => return vec![(c, chars.as_str())],
+            None => vec![],
+        };
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -10,5 +26,12 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    fn item() {
+        let input = "abc";
+        let item = Item {};
+        let result = item.parse(input);
+        assert_eq!(result, vec![('a', "bc")]);
     }
 }
