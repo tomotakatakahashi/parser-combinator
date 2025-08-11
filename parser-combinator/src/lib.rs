e8674da70e890ac6ef09e84d0539232036efc264
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2025-08-11 23:29:14 +0900

    WIP migration

--- parser-combinator/src/lib.rs
+++ parser-combinator/src/lib.rs
@@ -86,67 +86,35 @@ fn alt<T: 'static>(
     })
 }
 
-/*
-struct Token<P> {
-    parser: P,
+fn ret<S: 'static, T, F>(parser: Box<Parser<'static, S>>, converter: F) -> Box<Parser<'static, T>>
+where
+    F: Fn(S) -> T + 'static,
+{
+    Box::new(move |input: &str| match parser(input) {
+        None => None,
+        Some((v, out)) => Some((converter(v), out)),
+    })
 }
 
-impl<P, T> Parser<T> for Token<P>
-where
-    P: Parser<T>,
-{
-    fn parse<'a>(&self, input: &'a str) -> Option<(T, &'a str)> {
-        let space_parser = space();
-        let leading_spaces_trimmed = space_parser.parse(input).unwrap().1;
-        return match self.parser.parse(leading_spaces_trimmed) {
+fn token<T: 'static>(parser: Box<Parser<'static, T>>) -> Box<Parser<'static, T>> {
+    Box::new(move |input: &str| {
+        let leading_spaces_trimmed = space()(input).unwrap().1;
+        return match parser(leading_spaces_trimmed) {
             None => None,
             Some((v, out)) => {
-                let (_, rest) = space_parser.parse(out).unwrap();
+                let (_, rest) = space()(out).unwrap();
                 Some((v, rest))
             }
         };
-    }
+    })
 }
 
-struct Nat {}
-impl Parser<u32> for Nat {
-    fn parse<'a>(&self, input: &'a str) -> Option<(u32, &'a str)> {
-        let digits_parser = Many { parser: digit() };
-        match digits_parser.parse(input) {
-            None => None,
-            Some((v, out)) => {
-                let string_repr: String = v.into_iter().collect();
-                Some((string_repr.parse().unwrap(), out))
-            }
-        }
-    }
+fn nat() -> Box<Parser<'static, u32>> {
+    ret(many(digit()), |v| {
+        let string_repr: String = v.into_iter().collect();
+        string_repr.parse().unwrap()
+    })
 }
-
-struct Return<P, F, S, T>
-where
-    P: Parser<S>,
-    F: Fn(S) -> T,
-{
-    parser: P,
-    converter: F,
-    _phantom_s: PhantomData<S>,
-    _phantom_t: PhantomData<T>,
-}
-
-impl<P, F, S, T> Parser<T> for Return<P, F, S, T>
-where
-    P: Parser<S>,
-    F: Fn(S) -> T,
-{
-    fn parse<'a>(&self, input: &'a str) -> Option<(T, &'a str)> {
-        return match self.parser.parse(input) {
-            None => None,
-            Some((v, out)) => Some(((self.converter)(v), out)),
-        };
-    }
-}
-
-*/
 
 /*
 Expr = Add Term Expr | Val Term
@@ -225,44 +193,14 @@ mod tests {
         assert_eq!(alt_parser("~3"), None);
     }
 
-    /*
     #[test]
-    fn token() {
-        let parser = Token {
-            parser: Many { parser: digit() },
-        };
-        assert_eq!(parser.parse("  12  a"), Some((vec!['1', '2'], "a")));
+    fn token_works() {
+        let parser = token(many(digit()));
+        assert_eq!(parser("  12  a"), Some((vec!['1', '2'], "a")));
     }
 
     #[test]
     fn nat_works() {
-        let parser = Nat {};
-        assert_eq!(parser.parse("123abc"), Some((123, "abc")));
+        assert_eq!(nat()("123abc"), Some((123, "abc")));
     }
-
-    #[test]
-    fn return_works() {
-        let factor_parser = Alt {
-            parser1: Return {
-                parser: Seq {
-                    parser1: Token {
-                        parser: char_parser('('),
-                    },
-                    parser2: Seq {
-                        parser1: Token { parser: Nat {} },
-                        parser2: Token {
-                            parser: char_parser(')'),
-                        },
-                    },
-                },
-                converter: |c| c.1 .0,
-                _phantom_s: PhantomData,
-                _phantom_t: PhantomData,
-            },
-            parser2: Token { parser: Nat {} },
-        };
-
-        assert_eq!(factor_parser.parse(" ( 123 )"), Some((123, "")));
-        assert_eq!(factor_parser.parse("123"), Some((123, "")));
-    }*/
 }
